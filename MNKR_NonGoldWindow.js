697c6c6ad6de9004497163ec3b98fef0e18e2ce8
munokura <munokura@users.noreply.github.com>
2020-08-21 06:03:53 +0900

    Release

---
+++ MNKR_NonGoldWindow.js
@@ -0,0 +1,28 @@
+﻿/*
+ * --------------------------------------------------
+ * MNKR_NonGoldWindow Ver.1.0.0
+ * Copyright (c) 2020 Munokura
+ * This software is released under the MIT license.
+ * http://opensource.org/licenses/mit-license.php
+ * --------------------------------------------------
+ */
+
+/*:
+ * @target MZ MV
+ * @url https://raw.githubusercontent.com/munokura/MNKR-MV-plugins/master/MNKR_NonGoldWindow.js
+ * @plugindesc メインメニューで所持金ウィンドウを非表示にします。
+ * @author munokura
+ *
+ * @help
+ * メインメニューで所持金ウィンドウを非表示にします。
+ *
+ * プラグインコマンドや設定はありません。
+ */
+
+(function(){
+	'use strict';
+
+	Scene_Menu.prototype.createGoldWindow = function() {
+	};
+
+})();
