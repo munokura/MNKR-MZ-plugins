4b64d7cc703ff14f36711c9361a3ec899c4fe230
munokura <munokura@users.noreply.github.com>
2020-08-21 06:01:24 +0900

    Release

---
+++ MNKR_GoldIcon.js
@@ -0,0 +1,40 @@
+/*
+ * --------------------------------------------------
+ * MNKR_GoldIcon Ver.1.0.0
+ * Copyright (c) 2020 Munokura
+ * This software is released under the MIT license.
+ * http://opensource.org/licenses/mit-license.php
+ * --------------------------------------------------
+ */
+
+/*:
+ * @target MZ MV
+ * @url https://raw.githubusercontent.com/munokura/MNKR-MV-plugins/master/MNKR_GoldIcon.js
+ * @plugindesc 通貨単位の表示をアイコンに置き換えます
+ * @author munokura
+ *
+ * @param Gold Icon
+ * @text 通貨アイコンID
+ * @type string
+ * @desc 通貨の単位に使用するアイコンID
+ * @default 313
+ *
+ * @help
+ * 通貨単位の表示をアイコンに置き換えます。
+ *
+ * プラグインコマンドはありません。
+ */
+
+(function() {
+	'use strict';
+
+    const parameters = PluginManager.parameters('MNKR_GoldIcon');
+	const goldIcon = parseInt(parameters['Gold Icon'] || 313);
+
+	Window_Base.prototype.drawCurrencyValue = function(value, unit, x, y, width) {
+		this.resetTextColor();
+		this.drawText(value, x, y, width - 36 - 6, 'right');
+		this.drawIcon(goldIcon, x + width - 36, y);
+	};
+
+})();
