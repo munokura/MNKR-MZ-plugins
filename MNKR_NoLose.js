fad23975585f40c5af2594b4ebceb27d19867079
munokura <36838778+munokura@users.noreply.github.com>
2020-08-26 12:54:14 +0900

    動作するスイッチとコモンイベントを追加

--- MNKR_NoLose.js
+++ MNKR_NoLose.js
@@ -1,6 +1,6 @@
 /*
  * --------------------------------------------------
- * MNKR_NoLose Ver.1.1.0
+ * MNKR_NoLose Ver.1.2.0
  * Copyright (c) 2020 Munokura
  * This software is released under the MIT license.
  * http://opensource.org/licenses/mit-license.php
@@ -8,22 +8,50 @@
  */
 
 /*:
- * @target MZ
+ * @target MZ MV
  * @url https://raw.githubusercontent.com/munokura/MNKR-MZ-plugins/master/MNKR_NoLose.js
  * @plugindesc 戦闘で全滅してもゲームオーバーになりません。
  * @author munokura
  *
+ * @param Switch Id
+ * @text スイッチID
+ * @type switch
+ * @desc 発動させるスイッチID
+ * @default 11
+ *
+ * @param Common Id
+ * @type common_event
+ * @text コモンイベントID
+ * @desc 全滅時に実行するコモンイベント
+ * @default 0
+ * 
  * @help
- * 戦闘で全滅してもゲームオーバーになりません。
+ * パラメーターで指定したスイッチがONの時、
+ * 戦闘で全滅するとゲームオーバーにならず、
+ * マップ画面へ移行後にコモンイベントを実行します。
  */
 
 (function() {
-    'use strict';
+    'use strict'
+
+    const parameters = PluginManager.parameters('MNKR_NoLose')
+    const switchId = Number(parameters['Switch Id'] || 11)
+    const commonId = Number(parameters['Common Id'] || 0)
 
     const _BattleManager_setup = BattleManager.setup
     BattleManager.setup = function(troopId, canEscape, canLose) {
-        _BattleManager_setup.apply(this, arguments);
-        this._canLose = true;
-    };
+        _BattleManager_setup.apply(this, arguments)
+        if ($gameSwitches.value(switchId)) {
+            this._canLose = true
+        }
+    }
 
-})();
+    const _BattleManager_updateBattleEnd = BattleManager.updateBattleEnd
+    BattleManager.updateBattleEnd = function() {
+        _BattleManager_updateBattleEnd.call(this)
+        if ($gameSwitches.value(switchId) && commonId > 0) {
+            $gameTemp.reserveCommonEvent(commonId)
+        }
+    }
+
+})()
