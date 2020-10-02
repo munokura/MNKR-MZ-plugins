d6942db1361f3888070dbf93aecabf4d9d1eff83
munokura <36838778+munokura@users.noreply.github.com>
2020-10-02 14:20:26 +0900

    Release

--- MNKR_VariableCommand.js
+++ MNKR_VariableCommand.js
@@ -1,6 +1,6 @@
 /*
  * --------------------------------------------------
- * MNKR_VariableCommand
+ * MNKR_VariableCommand Ver.1.0.0
  * Copyright (c) 2020 Munokura
  * This software is released under the MIT license.
  * http://opensource.org/licenses/mit-license.php
@@ -10,18 +10,34 @@
 /*:
  * @target MZ
  * @url https://raw.githubusercontent.com/munokura/MNKR-MZ-plugins/master/MNKR_VariableCommand.js
- * @plugindesc (試作中) 変数が使用できないイベントコマンドをプラグインコマンドで可能にします。
+ * @plugindesc 変数が使用できないイベントコマンドをプラグインコマンドで使用可能にします。
  * @author munokura
  * 
+ * @help
+ * 変数が使用できないイベントコマンドをプラグインコマンドで変数使用可能にします。
+ * 
+ * 以下が実装されています。
+ *   - メンバーを加える
+ *   - メンバーを外す
+ *   - 職業を変更する
+ *   - 名前入力の処理
+ *
+ * 
+ * 利用規約:
+ *   MITライセンスです。
+ *   https://ja.osdn.net/projects/opensource/wiki/licenses%2FMIT_license
+ *   作者に無断で改変、再配布が可能で、
+ *   利用形態（商用、18禁利用等）についても制限はありません。
+ *
  * @command addActor
  * @text メンバーを加える
  * @desc 変数の値のアクターIDをメンバーに加えます。
  *
- * @arg variableID
+ * @arg variableIdActor
  * @text アクターIDを指定する変数ID
  * @desc 変数の値をアクターIDとして実行します。
  * @type variable
- * @min 1
+ * @default 1
  * 
  * @arg setup
  * @text アクター初期化
@@ -36,11 +52,11 @@
  * @text メンバーを外す
  * @desc 変数の値のアクターIDをメンバーから外します。
  *
- * @arg variableID
+ * @arg variableIdActor
  * @text アクターIDを指定する変数ID
  * @desc 変数の値をアクターIDとして実行します。
  * @type variable
- * @min 1
+ * @default 1
  * 
  * @arg setup
  * @text アクター初期化
@@ -51,17 +67,47 @@
  * @default false
  * 
  * 
- * 
- * @help
- * 試作中です。
- * 
- * 変数が使用できないイベントコマンドをプラグインコマンドで可能にします。
+ * @command changeClass
+ * @text 職業を変更する
+ * @desc 変数の値のアクターIDの職業を変更します。
  *
- * 利用規約:
- *   MITライセンスです。
- *   https://ja.osdn.net/projects/opensource/wiki/licenses%2FMIT_license
- *   作者に無断で改変、再配布が可能で、
- *   利用形態（商用、18禁利用等）についても制限はありません。
+ * @arg variableIdActor
+ * @text アクターIDを指定する変数ID
+ * @desc 変数の値をアクターIDとして実行します。
+ * @type variable
+ * @default 1
+ * 
+ * @arg variableIdClass
+ * @text 職業IDを指定する変数ID
+ * @desc 変数の値を職業IDとして実行します。
+ * @type variable
+ * @default 2
+ * 
+ * @arg setup
+ * @text レベルの保持
+ * @desc レベルを保持します。
+ * @type boolean
+ * @on 保持する
+ * @off 保持しない
+ * @default false
+ * 
+ * 
+ * @command nameInput
+ * @text 名前入力の処理
+ * @desc 変数の値のアクターIDの名前を入力します。
+ *
+ * @arg variableIdActor
+ * @text アクターIDを指定する変数ID
+ * @desc 変数の値をアクターIDとして実行します。
+ * @type variable
+ * @default 1
+ * 
+ * @arg maxWord
+ * @text 入力最大文字数
+ * @desc 入力できる最大文字数を指定します。
+ * @type number
+ * @min 1
+ * @default 8
  * 
  * 
  */
@@ -71,34 +117,43 @@
     "use strict";
     const pluginName = "MNKR_VariableCommand";
 
-    let variableID;
-    let variable;
-    let setup;
-
-    function variableCommandSetup(args) {
-        variableID = Number(args.variableID);
-        variable = $gameVariables.value(args.variableID);
-        setup = eval(args.setup);
-        return args;
-    };
-
     PluginManager.registerCommand(pluginName, "addActor", args => {
-        variableCommandSetup(args);
-        if (variable) {
-            if (setup) {
-                $gameActors.actor(variable).setup(variable);
+        const actorID = $gameVariables.value(Number(args.variableIdActor));
+        const doSetup = args.setup === "true";
+        if (actorID) {
+            if (doSetup) {
+                $gameActors.actor(actorID).setup(actorID);
             }
-            $gameParty.addActor(variable);
+            $gameParty.addActor(actorID);
         }
     });
 
     PluginManager.registerCommand(pluginName, "removeActor", args => {
-        variableCommandSetup(args);
-        if (variable) {
-            if (setup) {
-                $gameActors.actor(variable).setup(variable);
+        const actorID = $gameVariables.value(Number(args.variableIdActor));
+        const doSetup = args.setup === "true";
+        if (actorID) {
+            if (doSetup) {
+                $gameActors.actor(actorID).setup(actorID);
             }
-            $gameParty.removeActor(variable);
+            $gameParty.removeActor(actorID);
+        }
+    });
+
+    PluginManager.registerCommand(pluginName, "changeClass", args => {
+        const actorID = $gameVariables.value(Number(args.variableIdActor));
+        const classID = $gameVariables.value(Number(args.variableIdClass));
+        const doSetup = args.setup === "true";
+        if (actorID && classID) {
+            $gameActors.actor(actorID).changeClass(classID, doSetup);
+        }
+    });
+
+    PluginManager.registerCommand(pluginName, "nameInput", args => {
+        const actorID = $gameVariables.value(Number(args.variableIdActor));
+        const max = Number(args.maxWord);
+        if (actorID && max) {
+            SceneManager.push(Scene_Name);
+            SceneManager.prepareNextScene(actorID, max);
         }
     });
 
