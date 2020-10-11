b09ecfd87881bb952d889904822b6f6e25ffc75f
munokura <munokura@users.noreply.github.com>
2020-10-11 15:16:24 +0900

    コメントアウト箇所の削除

--- MNKR_TimingInvokeCommonMZ.js
+++ MNKR_TimingInvokeCommonMZ.js
@@ -19,9 +19,6 @@
 // author Yana
 //
 
-// var Imported = Imported || {};
-// Imported['TimingInvokeCommon'] = 1.00;
-
 /*:
  * @target MZ
  * @url https://raw.githubusercontent.com/munokura/MNKR-MZ-plugins/master/MNKR_TimingInvokeCommonMZ.js
