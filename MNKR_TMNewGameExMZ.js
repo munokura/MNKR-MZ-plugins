e9ffdc0807cc2c50fd28e68b64c58acaca4b88a1
munokura <munokura@users.noreply.github.com>
2020-10-12 19:54:55 +0900

    MZへ移植

---
+++ MNKR_TMNewGameExMZ.js
@@ -0,0 +1,199 @@
+﻿/*
+ * --------------------------------------------------
+ * MNKR_TMNewGameExMZ Ver.1.0.0
+ * Copyright (c) 2020 Munokura
+ * This software is released under the MIT license.
+ * http://opensource.org/licenses/mit-license.php
+ * --------------------------------------------------
+ */
+
+//=============================================================================
+// TMPlugin - 別マップでニューゲーム
+// バージョン: 2.0.0
+// 最終更新日: 2017/10/04
+// 配布元    : http://hikimoki.sakura.ne.jp/
+//-----------------------------------------------------------------------------
+// Copyright (c) 2016 tomoaky
+// Released under the MIT license.
+// http://opensource.org/licenses/mit-license.php
+//=============================================================================
+
+/*:
+ * @target MZ
+ * @url https://raw.githubusercontent.com/munokura/MNKR-MZ-plugins/master/MNKR_TMNewGameExMZ.js
+ * @plugindesc タイトルコマンドに別マップからゲームを始めるコマンドを追加します。
+ * @author tomoaky (改変 munokura)
+ *
+ * @param titleCommands
+ * @text タイトルコマンドの並び順
+ * @type string
+ * @desc タイトルコマンドの並び順
+ * 初期値: newGame continue options ex1
+ * @default newGame continue options ex1
+ * 
+ * @param command1
+ * @text 追加コマンド 1 の項目名
+ * @type string
+ * @desc 追加コマンド 1 の項目名
+ * 初期値: 別マップから
+ * @default 別マップから
+ *
+ * @param params1
+ * @text 追加コマンド 1 のパラメータ
+ * @type string
+ * @desc 追加コマンド 1 のパラメータ
+ * 初期値: 1 4 5 6
+ * @default 1 4 5 6
+ *
+ * @param command2
+ * @text 追加コマンド 2 の項目名
+ * @type string
+ * @desc 追加コマンド 2 の項目名
+ * 初期値: 
+ * @default 
+ *
+ * @param params2
+ * @text 追加コマンド 2 のパラメータ
+ * @type string
+ * @desc 追加コマンド 2 のパラメータ
+ * 初期値: 
+ * @default 
+ *
+ * @param command3
+ * @text 追加コマンド 3 の項目名
+ * @type string
+ * @desc 追加コマンド 3 の項目名
+ * 初期値: 
+ * @default 
+ *
+ * @param params3
+ * @text 追加コマンド 3 のパラメータ
+ * @type string
+ * @desc 追加コマンド 3 のパラメータ
+ * 初期値: 
+ * @default 
+ *
+ * @param command4
+ * @text 追加コマンド 4 の項目名
+ * @type string
+ * @desc 追加コマンド 4 の項目名
+ * 初期値: 
+ * @default 
+ *
+ * @param params4
+ * @text 追加コマンド 4 のパラメータ
+ * @type string
+ * @desc 追加コマンド 4 のパラメータ
+ * 初期値: 
+ * @default 
+ *
+ * @param command5
+ * @text 追加コマンド 5 の項目名
+ * @type string
+ * @desc 追加コマンド 5 の項目名
+ * 初期値: 
+ * @default 
+ *
+ * @param params5
+ * @text 追加コマンド 5 のパラメータ
+ * @type string
+ * @desc 追加コマンド 5 のパラメータ
+ * 初期値: 
+ * @default 
+ * 
+ * @help
+ * 使い方:
+ *
+ *   プラグインパラメータ titleCommands に ex1 ～ ex5 までを
+ *   必要な分だけ追加してください。(半角スペースで区切る)
+ *   continue や options を削除すれば、既存のコマンドを
+ *   除外することもできます。
+ * 
+ *   次に、追加したいコマンド名とパラメータを設定してください、
+ *   ex1 なら command1 と params1 の設定が必要になります。
+ *   params1 に以下4要素を半角スペースで区切って入力します。
+ *     マップ番号 X座標 Y座標 プレイヤーの向き
+ *
+ *   プレイヤーの向きは 下=2 左=4 右=6 上=8 となっています。
+ *
+ *  
+ *   プラグインコマンドはありません。
+ *
+ * 
+ * 類似機能をもつプラグインとの併用:
+ * 
+ *   タイトルコマンドを追加するプラグインと併用する場合、titleCommands に
+ *   他プラグインで追加されるコマンドのsymbol(シンボル)を追加してください。
+ *   (symbol は addCommand の 2 番目の引数の値です)
+ * 
+ *   その際、MNKR_TMNewGameExMZ.js は他プラグインよりも下に導入してください。
+ * 
+ * 
+ * 利用規約:
+ *   MITライセンスです。
+ *   https://ja.osdn.net/projects/opensource/wiki/licenses%2FMIT_license
+ *   作者に無断で改変、再配布が可能で、
+ *   利用形態（商用、18禁利用等）についても制限はありません。
+ */
+
+(() => {
+    'use strict';
+
+    const parameters = PluginManager.parameters('MNKR_TMNewGameExMZ');
+    const titleCommands = parameters['titleCommands'].split(' ');
+    const commands = {};
+    const transferParams = {};
+    for (let i = 1; i < 6; i++) {
+        commands['ex' + i] = parameters['command' + i];
+        transferParams['ex' + i] = parameters['params' + i].split(' ').map(Number);
+    }
+
+    const _Window_TitleCommand_makeCommandList = Window_TitleCommand.prototype.makeCommandList;
+    Window_TitleCommand.prototype.makeCommandList = function() {
+        _Window_TitleCommand_makeCommandList.call(this);
+        let listEx = [];
+        for (let i = 0; i < titleCommands.length; i++) {
+            for (let j = 0; j < this._list.length; j++) {
+                if (titleCommands[i] === this._list[j].symbol) {
+                    listEx.push({
+                        name: this._list[j].name,
+                        symbol: this._list[j].symbol,
+                        enabled: this._list[j].enabled,
+                        ext: this._list[j].ext
+                    });
+                }
+            }
+            if (commands[titleCommands[i]]) {
+                listEx.push({
+                    name: commands[titleCommands[i]],
+                    symbol: titleCommands[i],
+                    enabled: true,
+                    ext: null
+                });
+            }
+        }
+        this._list = listEx;
+    };
+
+    //-----------------------------------------------------------------------------
+    // Scene_Title
+    //
+
+    const _Scene_Title_createCommandWindow = Scene_Title.prototype.createCommandWindow;
+    Scene_Title.prototype.createCommandWindow = function() {
+        _Scene_Title_createCommandWindow.call(this);
+        for (let i = 1; i < 6; i++) {
+            let symbol = 'ex' + i;
+            this._commandWindow.setHandler(symbol, this.commandNewGameEx.bind(this, symbol));
+        }
+    };
+
+    Scene_Title.prototype.commandNewGameEx = function(symbol) {
+        DataManager.setupNewGame();
+        $gamePlayer.reserveTransfer.apply($gamePlayer, transferParams[symbol]);
+        this._commandWindow.close();
+        this.fadeOutAll();
+        SceneManager.goto(Scene_Map);
+    };
+
+})();
---
+++ MNKR_TMTimerExMZ.js
@@ -0,0 +1,170 @@
+﻿/*
+ * --------------------------------------------------
+ * MNKR_TMTimerExMZ Ver.1.0.0
+ * Copyright (c) 2020 Munokura
+ * This software is released under the MIT license.
+ * http://opensource.org/licenses/mit-license.php
+ * --------------------------------------------------
+ */
+
+//=============================================================================
+// TMVplugin - タイマー拡張
+// 作者: tomoaky (http://hikimoki.sakura.ne.jp/)
+// Version: 1.0
+// 最終更新日: 2016/07/29
+//=============================================================================
+
+/*:
+ * @target MZ
+ * @url https://raw.githubusercontent.com/munokura/MNKR-MZ-plugins/master/MNKR_TMTimerExMZ.js
+ * @plugindesc タイマー表示を秒単位からミリ秒単位に変更します。
+ *
+ * @author tomoaky (改変 munokura)
+ *
+ * @param timerX
+ * @text タイマーX座標
+ * @type number
+ * @desc タイマーを表示するX座標。
+ * 初期値: 816(ウィンドウの幅以上の値の場合は右端)
+ * @default 816
+ *
+ * @param timerY
+ * @text タイマーY座標
+ * @type number
+ * @desc タイマーを表示するY座標。
+ * 初期値: 48(ウィンドウの高さ以上の値の場合は下端)
+ * @default 48
+ *
+ * @param showMinutes
+ * @text タイマー表示形式
+ * @type boolean
+ * @on 分:秒
+ * @off 秒のみ
+ * @desc タイマーを 分 : 秒 の書式で表示する。
+ * 初期値: true (false-秒のみ / true-分:秒)
+ * @default true
+ *
+ * @param msecShiftY
+ * @text ミリ秒のY調整
+ * @type number
+ * @desc ミリ秒表示のY座標を調整します。
+ * 初期値: 36
+ * @default 36
+ *
+ * @help
+ * 使用方法:
+ * 
+ *   プラグインを導入するだけでタイマー機能が拡張されます。
+ *
+ *   イベントコマンド『条件分岐』のタイマー判定を使用すると、
+ *   ミリ秒単位ではまだ時間が残っているのにも関わらず、
+ *   時間切れと判定されてしまいます。
+ *   プラグインコマンドでゲーム変数に残り時間を代入してから、
+ *   その変数を使って条件分岐を実行することでこの問題を回避できます。
+ *
+ *   ミリ秒表示は毎フレーム再描画を実行するため、処理が重くなります。
+ *
+ *
+ * プラグインコマンド:
+ *
+ *   timerMsec 5
+ *     ゲーム変数5番にタイマーの残り時間をミリ秒単位で代入します。
+ * 
+ * 
+ * 利用規約:
+ *   MITライセンスです。
+ *   https://ja.osdn.net/projects/opensource/wiki/licenses%2FMIT_license
+ *   作者に無断で改変、再配布が可能で、
+ *   利用形態（商用、18禁利用等）についても制限はありません。
+ * 
+ * 
+ * @command timerMsec
+ * @text タイマーの残り時間を代入
+ * @desc タイマーの残り時間を代入します。
+ *
+ * @arg variableId
+ * @text 変数ID
+ * @desc 変数にタイマーの残り時間をミリ秒単位で代入します。
+ * @type variable
+ * @default 0
+ */
+
+(() => {
+
+  "use strict";
+
+  const parameters = PluginManager.parameters('MNKR_TMTimerExMZ');
+  const timerX = Number(parameters['timerX'] || 816);
+  const timerY = Number(parameters['timerY'] || 48);
+  const showMinutes = eval(parameters['showMinutes'] || true);
+  const msecShiftY = Number(parameters['msecShiftY'] || 0);
+  const pluginName = "MNKR_TMTimerExMZ";
+
+  //-----------------------------------------------------------------------------
+  // Game_Timer
+  //
+
+  Game_Timer.prototype.msec = function () {
+    return Math.floor(this._frames / 60 * 1000);
+  };
+
+  //-----------------------------------------------------------------------------
+  // PluginManager
+  //
+
+  PluginManager.registerCommand(pluginName, "timerMsec", args => {
+    const variableId = Number(args.variableId);
+    if (variableId) {
+      $gameVariables.setValue(variableId, $gameTimer.msec());
+    }
+  });
+
+  //-----------------------------------------------------------------------------
+  // Sprite_Timer
+  //
+
+  Sprite_Timer.prototype.createBitmap = function () {
+    this.bitmap = new Bitmap(160, 48);
+  };
+
+  var _Sprite_Timer_updateBitmap = Sprite_Timer.prototype.updateBitmap;
+  Sprite_Timer.prototype.updateBitmap = function () {
+    _Sprite_Timer_updateBitmap.call(this);
+    var msec = $gameTimer.msec();
+    if (this._msec !== msec) {
+      this._msec = msec;
+      this.redrawMsec();
+    }
+  };
+
+  Sprite_Timer.prototype.redraw = function () {
+    this.bitmap.fontSize = 32;
+    var text = showMinutes ? this.timerText() : '' + this._seconds;
+    var height = this.bitmap.height;
+    this.bitmap.clearRect(0, 0, 96, height);
+    this.bitmap.drawText(text, 0, 0, 96, height, 'right');
+  };
+
+  Sprite_Timer.prototype.redrawMsec = function () {
+    this.bitmap.fontSize = 16;
+    var text = '.' + (this._msec % 1000).padZero(3);
+    var y = this.bitmap.height - msecShiftY;
+    var width = this.bitmap.width - 96;
+    this.bitmap.clearRect(96, y, width, msecShiftY);
+    this.bitmap.drawText(text, 96, y, width, msecShiftY, 'left');
+  };
+
+  Sprite_Timer.prototype.updatePosition = function () {
+    if (timerX >= Graphics.width) {
+      this.x = Graphics.width - this.bitmap.width;
+    } else {
+      this.x = timerX;
+    }
+    if (timerY >= Graphics.height) {
+      this.y = Graphics.height - this.bitmap.height;
+    } else {
+      this.y = timerY;
+    }
+  };
+
+})();
