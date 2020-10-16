4a4ca16ed4e9a363b8e5e7683f013ca6957cc175
munokura <36838778+munokura@users.noreply.github.com>
2020-10-16 15:31:11 +0900

    URL修正

--- MNKR_TMMoveExMZ.js
+++ MNKR_TMMoveExMZ.js
@@ -20,7 +20,7 @@
 
 /*:
  * @target MZ
- * @url https://raw.githubusercontent.com/munokura/MNKR-MZ-plugins/master/MNKR_TMByeCommandMZ.js
+ * @url https://raw.githubusercontent.com/munokura/MNKR-MZ-plugins/master/MNKR_TMMoveExMZ.js
  * @author tomoaky (改変 munokura)
  * @plugindesc 壁衝突音やリージョンによる通行設定などの機能を追加します。
  * 
