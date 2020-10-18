82afd01334157f4848df5b951579a19b038933ac
munokura <munokura@users.noreply.github.com>
2020-10-18 12:26:45 +0900

    リファクタ

--- MNKR_TMItemRestrictionMZ.js
+++ MNKR_TMItemRestrictionMZ.js
@@ -1,6 +1,6 @@
 ﻿/*
  * --------------------------------------------------
- * MNKR_TMItemRestrictionMZ Ver.1.0.2
+ * MNKR_TMItemRestrictionMZ Ver.1.0.1
  * Copyright (c) 2020 Munokura
  * This software is released under the MIT license.
  * http://opensource.org/licenses/mit-license.php
@@ -9,7 +9,7 @@
 
 //=============================================================================
 // TMPlugin - アイテム制約拡張
-// バージョン: 1.0.2
+// バージョン: 1.0.1
 // 最終更新日: 2017/02/14
 // 配布元    : http://hikimoki.sakura.ne.jp/
 //-----------------------------------------------------------------------------
@@ -70,26 +70,26 @@
 (() => {
 
     "use strict";
-    const pluginName = "MNKR_TMItemRestrictionMZ";
+    const pluginName = document.currentScript.src.split("/").pop().replace(/\.js$/, "");
 
     //-----------------------------------------------------------------------------
     // Game_BattlerBase
     //
 
-    Game_BattlerBase.prototype.clearItemRestriction = function() {
+    Game_BattlerBase.prototype.clearItemRestriction = function () {
         this._alreadyUsed = [];
     };
 
-    Game_BattlerBase.prototype.alreadyUsed = function() {
+    Game_BattlerBase.prototype.alreadyUsed = function () {
         if (this._alreadyUsed == null) this.clearItemRestriction();
         return this._alreadyUsed;
     };
 
-    Game_BattlerBase.prototype.isAlreadyUsed = function(item) {
+    Game_BattlerBase.prototype.isAlreadyUsed = function (item) {
         return this.alreadyUsed().contains(item.id);
     };
 
-    Game_BattlerBase.prototype.setAlreadyUsed = function(item) {
+    Game_BattlerBase.prototype.setAlreadyUsed = function (item) {
         this.alreadyUsed().push(item.id);
     };
 
@@ -98,7 +98,7 @@
     //
 
     const _Game_Actor_setup = Game_Actor.prototype.setup;
-    Game_Actor.prototype.setup = function(actorId) {
+    Game_Actor.prototype.setup = function (actorId) {
         _Game_Actor_setup.call(this, actorId);
         this.clearItemRestriction();
     };
@@ -108,18 +108,18 @@
     //
 
     const _Game_Action_testApply = Game_Action.prototype.testApply;
-    Game_Action.prototype.testApply = function(target) {
+    Game_Action.prototype.testApply = function (target) {
         if (this.isAlreadyUsed(target)) return false;
         if (!this.isTargetActorValid(target)) return false;
         return _Game_Action_testApply.call(this, target);
     };
 
-    Game_Action.prototype.isAlreadyUsed = function(target) {
+    Game_Action.prototype.isAlreadyUsed = function (target) {
         const item = this.item();
         return this.isItem() && item.meta.justOnce && target.isAlreadyUsed(item);
     };
 
-    Game_Action.prototype.isTargetActorValid = function(target) {
+    Game_Action.prototype.isTargetActorValid = function (target) {
         if (target.isActor() && this.isItem()) {
             const item = this.item();
             if (item.meta.targetActor) {
@@ -131,12 +131,12 @@
     };
 
     const _Game_Action_apply = Game_Action.prototype.apply;
-    Game_Action.prototype.apply = function(target) {
+    Game_Action.prototype.apply = function (target) {
         _Game_Action_apply.call(this, target);
         if (target.result().isHit()) this.setAlreadyUsed(target);
     };
 
-    Game_Action.prototype.setAlreadyUsed = function(target) {
+    Game_Action.prototype.setAlreadyUsed = function (target) {
         const item = this.item();
         if (this.isItem() && item.meta.justOnce) target.setAlreadyUsed(item);
     };
@@ -154,16 +154,16 @@
     // Window_BattleActor
     //
 
-    Window_BattleActor.prototype.playOkSound = function() {};
+    Window_BattleActor.prototype.playOkSound = function () { };
 
-    Window_BattleActor.prototype.playBuzzerSound = function() {};
+    Window_BattleActor.prototype.playBuzzerSound = function () { };
 
     //-----------------------------------------------------------------------------
     // Scene_Battle
     //
 
     const _Scene_Battle_onActorOk = Scene_Battle.prototype.onActorOk;
-    Scene_Battle.prototype.onActorOk = function() {
+    Scene_Battle.prototype.onActorOk = function () {
         const action = BattleManager.inputtingAction();
         const actor = this._actorWindow.actor(this._actorWindow.index());
         if (action.isAlreadyUsed(actor) || !action.isTargetActorValid(actor)) {
