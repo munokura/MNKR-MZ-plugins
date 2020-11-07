b9c85f34f412dd383a5cfcd1fe17f5f7df1c99e5
munokura <munokura@users.noreply.github.com>
2020-11-07 21:30:00 +0900

    本家準拠に変更

--- MNKR_TMNewGameExMZ.js
+++ MNKR_TMNewGameExMZ.js
@@ -1,6 +1,6 @@
-﻿/*
+/*
  * --------------------------------------------------
- * MNKR_TMNewGameExMZ Ver.1.0.0
+ * MNKR_TMNewGameExMZ Ver.1.0.1
  * Copyright (c) 2020 Munokura
  * This software is released under the MIT license.
  * http://opensource.org/licenses/mit-license.php
@@ -23,83 +23,6 @@
  * @url https://raw.githubusercontent.com/munokura/MNKR-MZ-plugins/master/MNKR_TMNewGameExMZ.js
  * @plugindesc タイトルコマンドに別マップからゲームを始めるコマンドを追加します。
  * @author tomoaky (改変 munokura)
- *
- * @param titleCommands
- * @text タイトルコマンドの並び順
- * @type string
- * @desc タイトルコマンドの並び順
- * 初期値: newGame continue options ex1
- * @default newGame continue options ex1
- * 
- * @param command1
- * @text 追加コマンド 1 の項目名
- * @type string
- * @desc 追加コマンド 1 の項目名
- * 初期値: 別マップから
- * @default 別マップから
- *
- * @param params1
- * @text 追加コマンド 1 のパラメータ
- * @type string
- * @desc 追加コマンド 1 のパラメータ
- * 初期値: 1 4 5 6
- * @default 1 4 5 6
- *
- * @param command2
- * @text 追加コマンド 2 の項目名
- * @type string
- * @desc 追加コマンド 2 の項目名
- * 初期値: 
- * @default 
- *
- * @param params2
- * @text 追加コマンド 2 のパラメータ
- * @type string
- * @desc 追加コマンド 2 のパラメータ
- * 初期値: 
- * @default 
- *
- * @param command3
- * @text 追加コマンド 3 の項目名
- * @type string
- * @desc 追加コマンド 3 の項目名
- * 初期値: 
- * @default 
- *
- * @param params3
- * @text 追加コマンド 3 のパラメータ
- * @type string
- * @desc 追加コマンド 3 のパラメータ
- * 初期値: 
- * @default 
- *
- * @param command4
- * @text 追加コマンド 4 の項目名
- * @type string
- * @desc 追加コマンド 4 の項目名
- * 初期値: 
- * @default 
- *
- * @param params4
- * @text 追加コマンド 4 のパラメータ
- * @type string
- * @desc 追加コマンド 4 のパラメータ
- * 初期値: 
- * @default 
- *
- * @param command5
- * @text 追加コマンド 5 の項目名
- * @type string
- * @desc 追加コマンド 5 の項目名
- * 初期値: 
- * @default 
- *
- * @param params5
- * @text 追加コマンド 5 のパラメータ
- * @type string
- * @desc 追加コマンド 5 のパラメータ
- * 初期値: 
- * @default 
  * 
  * @help
  * 使い方:
@@ -134,22 +57,108 @@
  *   https://ja.osdn.net/projects/opensource/wiki/licenses%2FMIT_license
  *   作者に無断で改変、再配布が可能で、
  *   利用形態（商用、18禁利用等）についても制限はありません。
+ *
+ * @param titleCommands
+ * @text タイトルコマンドの並び順
+ * @type string
+ * @desc タイトルコマンドの並び順
+ * 初期値: newGame continue options ex1
+ * @default newGame continue options ex1
+ * 
+ * @param command1
+ * @text 追加コマンド 1 の項目名
+ * @type string
+ * @desc 追加コマンド 1 の項目名
+ * 初期値: 別マップから
+ * @default 別マップから
+ *
+ * @param params1
+ * @text 追加コマンド 1 のパラメータ
+ * @type string
+ * @desc 要素を半角スペース区切りで入力。
+ * マップID X座標 Y座標 プレイヤーの向き
+ * @default 1 4 5 6
+ *
+ * @param command2
+ * @text 追加コマンド 2 の項目名
+ * @type string
+ * @desc 追加コマンド 2 の項目名
+ * 初期値: 
+ * @default 
+ *
+ * @param params2
+ * @text 追加コマンド 2 のパラメータ
+ * @type string
+ * @desc 要素を半角スペース区切りで入力。
+ * マップID X座標 Y座標 プレイヤーの向き
+ * @default 
+ *
+ * @param command3
+ * @text 追加コマンド 3 の項目名
+ * @type string
+ * @desc 追加コマンド 3 の項目名
+ * 初期値: 
+ * @default 
+ *
+ * @param params3
+ * @text 追加コマンド 3 のパラメータ
+ * @type string
+ * @desc 要素を半角スペース区切りで入力。
+ * マップID X座標 Y座標 プレイヤーの向き
+ * @default 
+ *
+ * @param command4
+ * @text 追加コマンド 4 の項目名
+ * @type string
+ * @desc 要素を半角スペース区切りで入力。
+ * マップID X座標 Y座標 プレイヤーの向き
+ * @default 
+ *
+ * @param params4
+ * @text 追加コマンド 4 のパラメータ
+ * @type string
+ * @desc 要素を半角スペース区切りで入力。
+ * マップID X座標 Y座標 プレイヤーの向き
+ * @default 
+ *
+ * @param command5
+ * @text 追加コマンド 5 の項目名
+ * @type string
+ * @desc 追加コマンド 5 の項目名
+ * 初期値: 
+ * @default 
+ *
+ * @param params5
+ * @text 追加コマンド 5 のパラメータ
+ * @type string
+ * @desc 要素を半角スペース区切りで入力。
+ * マップID X座標 Y座標 プレイヤーの向き
+ * @default 
  */
+
+var Imported = Imported || {};
+Imported.TMNewGameEx = true;
 
 (() => {
     'use strict';
 
-    const parameters = PluginManager.parameters('MNKR_TMNewGameExMZ');
+    const pluginName = document.currentScript.src.split("/").pop().replace(/\.js$/, "");
+    const parameters = PluginManager.parameters(pluginName);
     const titleCommands = parameters['titleCommands'].split(' ');
     const commands = {};
     const transferParams = {};
+
     for (let i = 1; i < 6; i++) {
         commands['ex' + i] = parameters['command' + i];
         transferParams['ex' + i] = parameters['params' + i].split(' ').map(Number);
     }
 
+    //-----------------------------------------------------------------------------
+    // Window_TitleCommand
+    //
+
     const _Window_TitleCommand_makeCommandList = Window_TitleCommand.prototype.makeCommandList;
-    Window_TitleCommand.prototype.makeCommandList = function() {
+    Window_TitleCommand.prototype.makeCommandList = function () {
         _Window_TitleCommand_makeCommandList.call(this);
         let listEx = [];
         for (let i = 0; i < titleCommands.length; i++) {
@@ -180,7 +189,7 @@
     //
 
     const _Scene_Title_createCommandWindow = Scene_Title.prototype.createCommandWindow;
-    Scene_Title.prototype.createCommandWindow = function() {
+    Scene_Title.prototype.createCommandWindow = function () {
         _Scene_Title_createCommandWindow.call(this);
         for (let i = 1; i < 6; i++) {
             let symbol = 'ex' + i;
@@ -188,7 +197,7 @@
         }
     };
 
-    Scene_Title.prototype.commandNewGameEx = function(symbol) {
+    Scene_Title.prototype.commandNewGameEx = function (symbol) {
         DataManager.setupNewGame();
         $gamePlayer.reserveTransfer.apply($gamePlayer, transferParams[symbol]);
         this._commandWindow.close();
