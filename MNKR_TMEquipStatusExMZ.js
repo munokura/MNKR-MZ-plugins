243e9f345c5666ce4dcd09be54d0bcbd0dc473af
munokura <munokura@users.noreply.github.com>
2020-11-07 18:40:02 +0900

    コード調整

--- MNKR_TMEquipStatusExMZ.js
+++ MNKR_TMEquipStatusExMZ.js
@@ -1,6 +1,6 @@
 ﻿/*
  * --------------------------------------------------
- * MNKR_TMEquipStatusExMZ Ver.1.0.0
+ * MNKR_TMEquipStatusExMZ Ver.1.0.1
  * Copyright (c) 2020 Munokura
  * This software is released under the MIT license.
  * http://opensource.org/licenses/mit-license.php
@@ -23,21 +23,6 @@
  * @url https://raw.githubusercontent.com/munokura/MNKR-MZ-plugins/master/MNKR_TMEquipStatusExMZ.js
  * @author tomoaky (改変 munokura)
  * @plugindesc 装備ステータスの項目を自由に編集します。
- *
- * @param paramList
- * @text 表示パラメータ
- * @type string
- * @desc 表示するパラメータ番号を半角スペースで区切って並べます。
- * 初期値: 0 1 2 3 4 5 6 7
- * @default 0 1 2 3 4 5 6 7
- *
- * @param face
- * @text 顔画像表示
- * @type boolean
- * @on 表示
- * @off 非表示
- * @desc 装備シーンに顔画像を表示します。画面に収まる表示パラメーターが7個までになります。
- * @default false
  * 
  * @help
  * 使い方:
@@ -63,24 +48,47 @@
  *   https://ja.osdn.net/projects/opensource/wiki/licenses%2FMIT_license
  *   作者に無断で改変、再配布が可能で、
  *   利用形態（商用、18禁利用等）についても制限はありません。
+ * 
+ *
+ * @param paramList
+ * @text 表示パラメータ
+ * @type string
+ * @desc 表示するパラメータ番号を半角スペースで区切って並べます。
+ * 初期値: 0 1 2 3 4 5 6 7
+ * @default 0 1 2 3 4 5 6 7
+ *
+ * @param face
+ * @text 顔画像表示
+ * @type boolean
+ * @on 表示
+ * @off 非表示
+ * @desc 装備シーンに顔画像を表示します。画面に収まる表示パラメーターが7個までになります。
+ * @default false
  */
+
+var Imported = Imported || {};
+Imported.TMEquipStatusEx = true;
 
 (() => {
   'use strict';
 
-  const pluginName = 'MNKR_TMEquipStatusExMZ';
+  const pluginName = document.currentScript.src.split("/").pop().replace(/\.js$/, "");
   const parameters = PluginManager.parameters(pluginName);
   const paramList = parameters['paramList'].split(' ').map(Number);
-  const face = eval(parameters['face'] || 'false');
+  const face = String(parameters['face']) === 'true';
 
   //-----------------------------------------------------------------------------
   // Window_EquipStatus
   //
 
+  Window_EquipStatus.prototype.numVisibleRows = function () {
+    return paramList.length + 1;
+  };
+
   Window_EquipStatus.prototype.refresh = function () {
     this.contents.clear();
     if (this._actor) {
-      const nameRect = this.itemLineRect(0);
+      let nameRect = this.itemLineRect(0);
       this.drawActorName(this._actor, nameRect.x, 0, nameRect.width);
       if (face) {
         this.drawActorFace(this._actor, nameRect.x, nameRect.height);
