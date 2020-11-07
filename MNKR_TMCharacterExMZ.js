846530608d0133e70fa97f0e19d0f7bc650b6700
munokura <munokura@users.noreply.github.com>
2020-11-07 13:27:02 +0900

    本家準拠に

--- MNKR_TMCharacterExMZ.js
+++ MNKR_TMCharacterExMZ.js
@@ -1,6 +1,6 @@
-﻿/*
+/*
  * --------------------------------------------------
- * MNKR_TMCharacterExMZ Ver.1.0.0
+ * MNKR_TMCharacterExMZ Ver.1.0.1
  * Copyright (c) 2020 Munokura
  * This software is released under the MIT license.
  * http://opensource.org/licenses/mit-license.php
@@ -206,6 +206,34 @@
  * @default 1.00
  * 
  * 
+ * @command chrScaleRate
+ * @text イベント拡大補正率
+ * @desc イベントを拡大補正率を指定します。拡大率とは別に乗算され、時間経過で等倍に戻ります。
+ *
+ * @arg eventId
+ * @text イベントID
+ * @desc 表示位置を変更するイベントID。
+ * -1:プレイヤー / 0:実行イベント / 1以上:番号のイベント
+ * @type number
+ * @decimals
+ * @min -1
+ * @default 0
+ * 
+ * @arg scaleX
+ * @text 横方向拡大補正率
+ * @desc イベント横方向の拡大補正率。
+ * @type number
+ * @decimals 2
+ * @default 1.00
+ * 
+ * @arg scaleY
+ * @text 縦方向拡大補正率
+ * @desc イベント縦方向の拡大補正率。
+ * @type number
+ * @decimals 2
+ * @default 1.00
+ * 
+ * 
  * @command chrClear
  * @text イベント設定解除
  * @desc 他のプラグインコマンドで指定した効果を解除します。
@@ -220,224 +248,235 @@
  * @default 0
  */
 
+var Imported = Imported || {};
+Imported.TMCharacterEx = true;
 var TMPlugin = TMPlugin || {};
 
 if (!TMPlugin.EventBase) {
-
-    TMPlugin.EventBase = true;
-    (() => {
-        "use strict";
-
-        const _Game_Event_setupPage = Game_Event.prototype.setupPage;
-        Game_Event.prototype.setupPage = function() {
-            _Game_Event_setupPage.call(this);
-            if (this._pageIndex >= 0) this.loadCommentParams();
-        };
-
-        Game_Event.prototype.loadCommentParams = function() {
-            this._commentParams = {};
-            let re = /<([^<>:]+)(:?)([^>]*)>/g;
-            let list = this.list();
-            for (let i = 0; i < list.length; i++) {
-                let command = list[i];
-                if (command && command.code == 108 || command.code == 408) {
-                    for (;;) {
-                        let match = re.exec(command.parameters[0]);
-                        if (match) {
-                            this._commentParams[match[1]] = match[2] === ':' ? match[3] : true;
-                        } else {
-                            break;
-                        }
-                    }
-                } else {
-                    break;
-                }
-            }
-        };
-
-        Game_Event.prototype.loadTagParam = function(paramName) {
-            return this._commentParams[paramName] || this.event().meta[paramName];
-        };
-
-    })();
-} // TMPlugin.EventBase
-
-
-(() => {
-
+  TMPlugin.EventBase = true;
+  (() => {
     "use strict";
-    const pluginName = 'MNKR_TMCharacterExMZ';
-    const parameters = PluginManager.parameters(pluginName);
-    const LandingAnimation = eval(parameters['landingAnimation'] || 'false');
-
-    //-----------------------------------------------------------------------------
-    // Game_CharacterBase
-    //
-
-    const _Game_CharacterBase_initMembers = Game_CharacterBase.prototype.initMembers;
-    Game_CharacterBase.prototype.initMembers = function() {
-        _Game_CharacterBase_initMembers.call(this);
-        this.initChrParams();
-    }
-
-    Game_CharacterBase.prototype.initChrParams = function() {
-        this._chrShiftX = 0;
-        this._chrShiftY = 0;
-        this._chrAngle = 0;
-        this._chrScaleX = 1.0;
-        this._chrScaleY = 1.0;
-        this._chrAnchorX = 0.5;
-        this._chrAnchorY = 1;
-        this._chrScaleRateX = 1;
-        this._chrScaleRateY = 1;
-    };
-
-    const _Game_CharacterBase_screenX = Game_CharacterBase.prototype.screenX;
-    Game_CharacterBase.prototype.screenX = function() {
-        return _Game_CharacterBase_screenX.call(this) + this._chrShiftX;
-    };
-
-    const _Game_CharacterBase_screenY = Game_CharacterBase.prototype.screenY;
-    Game_CharacterBase.prototype.screenY = function() {
-        return _Game_CharacterBase_screenY.call(this) + this._chrShiftY;
-    };
-
-    Game_CharacterBase.prototype.screenAngle = function() {
-        return this._chrAngle;
-    };
-
-    Game_CharacterBase.prototype.screenScaleX = function() {
-        return this._chrScaleX * this._chrScaleRateX;
-    };
-
-    Game_CharacterBase.prototype.screenScaleY = function() {
-        return this._chrScaleY * this._chrScaleRateY;
-    };
-
-    Game_CharacterBase.prototype.screenAnchorX = function() {
-        return this._chrAnchorX;
-    };
-
-    Game_CharacterBase.prototype.screenAnchorY = function() {
-        return this._chrAnchorY;
-    };
-
-    Game_CharacterBase.prototype.setChrShift = function(shiftX, shiftY) {
-        if (shiftX != null && shiftY != null) {
-            this._chrShiftX = +shiftX;
-            this._chrShiftY = +shiftY;
-        }
-    };
-
-    Game_CharacterBase.prototype.setChrAngle = function(angle) {
-        if (angle != null) {
-            this._chrAngle = angle * Math.PI / 180;
-        }
-    };
-
-    Game_CharacterBase.prototype.setChrScale = function(scaleX, scaleY) {
-        if (scaleX != null && scaleY != null) {
-            this._chrScaleX = +scaleX;
-            this._chrScaleY = +scaleY;
-        }
-    };
-
-    Game_CharacterBase.prototype.setChrScaleRate = function(scaleRateX, scaleRateY) {
-        if (scaleRateX != null && scaleRateY != null) {
-            this._chrScaleRateX = +scaleRateX;
-            this._chrScaleRateY = +scaleRateY;
-        }
-    };
-
-    Game_CharacterBase.prototype.setChrAnchor = function(anchorX, anchorY) {
-        if (anchorX != null && anchorY != null) {
-            this._chrAnchorX = +anchorX;
-            this._chrAnchorY = +anchorY;
-        }
-    };
-
-    const _Game_CharacterBase_updateJump = Game_CharacterBase.prototype.updateJump;
-    Game_CharacterBase.prototype.updateJump = function() {
-        _Game_CharacterBase_updateJump.call(this);
-        if (LandingAnimation && this._jumpCount === 0) {
-            this.setChrScaleRate(1.3, 0.7);
-        }
-    };
-
-    const _Game_CharacterBase_updateAnimation = Game_CharacterBase.prototype.updateAnimation;
-    Game_CharacterBase.prototype.updateAnimation = function() {
-        _Game_CharacterBase_updateAnimation.call(this);
-        if (this._chrScaleRateX < 1) {
-            this._chrScaleRateX = Math.min(this._chrScaleRateX + 0.05, 1);
-        } else if (this._chrScaleRateX > 1) {
-            this._chrScaleRateX = Math.max(this._chrScaleRateX - 0.05, 1);
-        }
-        if (this._chrScaleRateY < 1) {
-            this._chrScaleRateY = Math.min(this._chrScaleRateY + 0.05, 1);
-        } else if (this._chrScaleRateY > 1) {
-            this._chrScaleRateY = Math.max(this._chrScaleRateY - 0.05, 1);
-        }
-    };
-
-    //-----------------------------------------------------------------------------
-    // Game_Event
-    //
 
     const _Game_Event_setupPage = Game_Event.prototype.setupPage;
-    Game_Event.prototype.setupPage = function() {
-        _Game_Event_setupPage.call(this);
-        if (this._pageIndex >= 0) {
-            const chrShift = this.loadTagParam('chrShift');
-            if (chrShift) this.setChrShift.apply(this, chrShift.split(' '));
-            this.setChrAngle(this.loadTagParam('chrAngle'));
-            const chrScale = this.loadTagParam('chrScale');
-            if (chrScale) this.setChrScale.apply(this, chrScale.split(' '));
-            const chrAnchor = this.loadTagParam('chrAnchor');
-            if (chrAnchor) this.setChrAnchor.apply(this, chrAnchor.split(' '));
+    Game_Event.prototype.setupPage = function () {
+      _Game_Event_setupPage.call(this);
+      if (this._pageIndex >= 0) this.loadCommentParams();
+    };
+
+    Game_Event.prototype.loadCommentParams = function () {
+      this._commentParams = {};
+      let re = /<([^<>:]+)(:?)([^>]*)>/g;
+      let list = this.list();
+      for (let i = 0; i < list.length; i++) {
+        let command = list[i];
+        if (command && command.code == 108 || command.code == 408) {
+          for (; ;) {
+            let match = re.exec(command.parameters[0]);
+            if (match) {
+              this._commentParams[match[1]] = match[2] === ':' ? match[3] : true;
+            } else {
+              break;
+            }
+          }
         } else {
-            this.initChrParams();
+          break;
         }
+      }
     };
 
-    PluginManager.registerCommand(pluginName, "chrShift", function(args) {
-        const offsetX = Number(args.offsetX);
-        const offsetY = Number(args.offsetY);
-        const character = this.character(Number(args.eventId));
-        if (character) character.setChrShift(offsetX, offsetY);
-    });
-
-    PluginManager.registerCommand(pluginName, "chrAngle", function(args) {
-        const angle = Number(args.angle);
-        const character = this.character(Number(args.eventId));
-        if (character) character.setChrAngle(angle);
-    });
-
-    PluginManager.registerCommand(pluginName, "chrScale", function(args) {
-        const scaleX = Number(args.scaleX);
-        const scaleY = Number(args.scaleY);
-        const character = this.character(Number(args.eventId));
-        if (character) character.setChrScale(scaleX, scaleY);
-    });
-
-    PluginManager.registerCommand(pluginName, "chrClear", function(args) {
-        const character = this.character(Number(args.eventId));
-        if (character) {
-            character.setChrShift(0, 0);
-            character.setChrAngle(0);
-            character.setChrScale(1.0, 1.0);
-        }
-    });
-
-    //-----------------------------------------------------------------------------
-    // Sprite_Character
-    //
-
-    const _Sprite_Character_updateOther = Sprite_Character.prototype.updateOther;
-    Sprite_Character.prototype.updateOther = function() {
-        _Sprite_Character_updateOther.call(this);
-        this.rotation = this._character.screenAngle();
-        this.scale.set(this._character.screenScaleX(), this._character.screenScaleY());
-        this.anchor.set(this._character.screenAnchorX(), this._character.screenAnchorY());
+    Game_Event.prototype.loadTagParam = function (paramName) {
+      return this._commentParams[paramName] || this.event().meta[paramName];
     };
+
+  })();
+} // TMPlugin.EventBase
+
+(() => {
+  "use strict";
+
+  const pluginName = document.currentScript.src.split("/").pop().replace(/\.js$/, "");
+
+  TMPlugin.CharacterEx = {};
+  TMPlugin.CharacterEx.Parameters = PluginManager.parameters(pluginName);
+  TMPlugin.CharacterEx.LandingAnimation = TMPlugin.CharacterEx.Parameters['landingAnimation'] === 'true';
+
+  //-----------------------------------------------------------------------------
+  // Game_CharacterBase
+  //
+
+  const _Game_CharacterBase_initMembers = Game_CharacterBase.prototype.initMembers;
+  Game_CharacterBase.prototype.initMembers = function () {
+    _Game_CharacterBase_initMembers.call(this);
+    this.initChrParams();
+  }
+
+  Game_CharacterBase.prototype.initChrParams = function () {
+    this._chrShiftX = 0;
+    this._chrShiftY = 0;
+    this._chrAngle = 0;
+    this._chrScaleX = 1.0;
+    this._chrScaleY = 1.0;
+    this._chrAnchorX = 0.5;
+    this._chrAnchorY = 1;
+    this._chrScaleRateX = 1;
+    this._chrScaleRateY = 1;
+  };
+
+  const _Game_CharacterBase_screenX = Game_CharacterBase.prototype.screenX;
+  Game_CharacterBase.prototype.screenX = function () {
+    return _Game_CharacterBase_screenX.call(this) + this._chrShiftX;
+  };
+
+  const _Game_CharacterBase_screenY = Game_CharacterBase.prototype.screenY;
+  Game_CharacterBase.prototype.screenY = function () {
+    return _Game_CharacterBase_screenY.call(this) + this._chrShiftY;
+  };
+
+  Game_CharacterBase.prototype.screenAngle = function () {
+    return this._chrAngle;
+  };
+
+  Game_CharacterBase.prototype.screenScaleX = function () {
+    return this._chrScaleX * this._chrScaleRateX;
+  };
+
+  Game_CharacterBase.prototype.screenScaleY = function () {
+    return this._chrScaleY * this._chrScaleRateY;
+  };
+
+  Game_CharacterBase.prototype.screenAnchorX = function () {
+    return this._chrAnchorX;
+  };
+
+  Game_CharacterBase.prototype.screenAnchorY = function () {
+    return this._chrAnchorY;
+  };
+
+  Game_CharacterBase.prototype.setChrShift = function (shiftX, shiftY) {
+    if (shiftX != null && shiftY != null) {
+      this._chrShiftX = +shiftX;
+      this._chrShiftY = +shiftY;
+    }
+  };
+
+  Game_CharacterBase.prototype.setChrAngle = function (angle) {
+    if (angle != null) {
+      this._chrAngle = angle * Math.PI / 180;
+    }
+  };
+
+  Game_CharacterBase.prototype.setChrScale = function (scaleX, scaleY) {
+    if (scaleX != null && scaleY != null) {
+      this._chrScaleX = +scaleX;
+      this._chrScaleY = +scaleY;
+    }
+  };
+
+  Game_CharacterBase.prototype.setChrScaleRate = function (scaleRateX, scaleRateY) {
+    if (scaleRateX != null && scaleRateY != null) {
+      this._chrScaleRateX = +scaleRateX;
+      this._chrScaleRateY = +scaleRateY;
+    }
+  };
+
+  Game_CharacterBase.prototype.setChrAnchor = function (anchorX, anchorY) {
+    if (anchorX != null && anchorY != null) {
+      this._chrAnchorX = +anchorX;
+      this._chrAnchorY = +anchorY;
+    }
+  };
+
+  const _Game_CharacterBase_updateJump = Game_CharacterBase.prototype.updateJump;
+  Game_CharacterBase.prototype.updateJump = function () {
+    _Game_CharacterBase_updateJump.call(this);
+    if (TMPlugin.CharacterEx.LandingAnimation && this._jumpCount === 0) {
+      this.setChrScaleRate(1.3, 0.7);
+    }
+  };
+
+  const _Game_CharacterBase_updateAnimation = Game_CharacterBase.prototype.updateAnimation;
+  Game_CharacterBase.prototype.updateAnimation = function () {
+    _Game_CharacterBase_updateAnimation.call(this);
+    if (this._chrScaleRateX < 1) {
+      this._chrScaleRateX = Math.min(this._chrScaleRateX + 0.05, 1);
+    } else if (this._chrScaleRateX > 1) {
+      this._chrScaleRateX = Math.max(this._chrScaleRateX - 0.05, 1);
+    }
+    if (this._chrScaleRateY < 1) {
+      this._chrScaleRateY = Math.min(this._chrScaleRateY + 0.05, 1);
+    } else if (this._chrScaleRateY > 1) {
+      this._chrScaleRateY = Math.max(this._chrScaleRateY - 0.05, 1);
+    }
+  };
+
+  //-----------------------------------------------------------------------------
+  // Game_Event
+  //
+
+  const _Game_Event_setupPage = Game_Event.prototype.setupPage;
+  Game_Event.prototype.setupPage = function () {
+    _Game_Event_setupPage.call(this);
+    if (this._pageIndex >= 0) {
+      let chrShift = this.loadTagParam('chrShift');
+      if (chrShift) this.setChrShift.apply(this, chrShift.split(' '));
+      this.setChrAngle(this.loadTagParam('chrAngle'));
+      let chrScale = this.loadTagParam('chrScale');
+      if (chrScale) this.setChrScale.apply(this, chrScale.split(' '));
+      let chrAnchor = this.loadTagParam('chrAnchor');
+      if (chrAnchor) this.setChrAnchor.apply(this, chrAnchor.split(' '));
+    } else {
+      this.initChrParams();
+    }
+  };
+
+  //-----------------------------------------------------------------------------
+  // PluginManager
+  //
+
+  PluginManager.registerCommand(pluginName, "chrShift", function (args) {
+    const arr = [args.eventId, args.offsetX, args.offsetY];
+    let character = this.character(+arr[0]);
+    if (character) character.setChrShift(arr[1], arr[2]);
+  });
+
+  PluginManager.registerCommand(pluginName, "chrAngle", function (args) {
+    const arr = [args.eventId, args.angle];
+    let character = this.character(+arr[0]);
+    if (character) character.setChrAngle(arr[1]);
+  });
+
+  PluginManager.registerCommand(pluginName, "chrScale", function (args) {
+    const arr = [args.eventId, args.scaleX, args.scaleY];
+    let character = this.character(+arr[0]);
+    if (character) character.setChrScale(arr[1], arr[2]);
+  });
+
+  PluginManager.registerCommand(pluginName, "chrScaleRate", function (args) {
+    const arr = [args.eventId, args.scaleX, args.scaleY];
+    let character = this.character(+arr[0]);
+    if (character) character.setChrScaleRate(arr[1], arr[2]);
+  });
+
+  PluginManager.registerCommand(pluginName, "chrClear", function (args) {
+    const arr = [args.eventId];
+    let character = this.character(+arr[0]);
+    if (character) {
+      character.setChrShift(0, 0);
+      character.setChrAngle(0);
+      character.setChrScale(1.0, 1.0);
+    }
+  });
+
+  //-----------------------------------------------------------------------------
+  // Sprite_Character
+  //
+
+  const _Sprite_Character_updateOther = Sprite_Character.prototype.updateOther;
+  Sprite_Character.prototype.updateOther = function () {
+    _Sprite_Character_updateOther.call(this);
+    this.rotation = this._character.screenAngle();
+    this.scale.set(this._character.screenScaleX(), this._character.screenScaleY());
+    this.anchor.set(this._character.screenAnchorX(), this._character.screenAnchorY());
+  };
 
 })();
