4502d998b1cdd56b37b4e01bf81c049862922a30
munokura <munokura@users.noreply.github.com>
2020-11-07 18:56:35 +0900

    本家準拠に変更

--- MNKR_TMItemRestrictionMZ.js
+++ MNKR_TMItemRestrictionMZ.js
@@ -1,6 +1,6 @@
-﻿/*
+/*
  * --------------------------------------------------
- * MNKR_TMItemRestrictionMZ Ver.1.0.2
+ * MNKR_TMItemRestrictionMZ Ver.1.0.3
  * Copyright (c) 2020 Munokura
  * This software is released under the MIT license.
  * http://opensource.org/licenses/mit-license.php
@@ -67,112 +67,121 @@
  * @default 0
  */
 
+var Imported = Imported || {};
+Imported.TMItemRestriction = true;
+
+var TMPlugin = TMPlugin || {};
+
 (() => {
 
-    "use strict";
-    const pluginName = document.currentScript.src.split("/").pop().replace(/\.js$/, "");
+  "use strict";
 
-    //-----------------------------------------------------------------------------
-    // Game_BattlerBase
-    //
+  const pluginName = document.currentScript.src.split("/").pop().replace(/\.js$/, "");
 
-    Game_BattlerBase.prototype.clearItemRestriction = function () {
-        this._alreadyUsed = [];
-    };
+  //-----------------------------------------------------------------------------
+  // Game_BattlerBase
+  //
 
-    Game_BattlerBase.prototype.alreadyUsed = function () {
-        if (this._alreadyUsed == null) this.clearItemRestriction();
-        return this._alreadyUsed;
-    };
+  Game_BattlerBase.prototype.clearItemRestriction = function () {
+    this._alreadyUsed = [];
+  };
 
-    Game_BattlerBase.prototype.isAlreadyUsed = function (item) {
-        return this.alreadyUsed().contains(item.id);
-    };
+  Game_BattlerBase.prototype.alreadyUsed = function () {
+    if (this._alreadyUsed == null) this.clearItemRestriction();
+    return this._alreadyUsed;
+  };
 
-    Game_BattlerBase.prototype.setAlreadyUsed = function (item) {
-        this.alreadyUsed().push(item.id);
-    };
+  Game_BattlerBase.prototype.isAlreadyUsed = function (item) {
+    return this.alreadyUsed().contains(item.id);
+  };
 
-    //-----------------------------------------------------------------------------
-    // Game_Actor
-    //
+  Game_BattlerBase.prototype.setAlreadyUsed = function (item) {
+    this.alreadyUsed().push(item.id);
+  };
 
-    const _Game_Actor_setup = Game_Actor.prototype.setup;
-    Game_Actor.prototype.setup = function (actorId) {
-        _Game_Actor_setup.call(this, actorId);
-        this.clearItemRestriction();
-    };
+  //-----------------------------------------------------------------------------
+  // Game_Actor
+  //
 
-    //-----------------------------------------------------------------------------
-    // Game_Action
-    //
+  const _Game_Actor_setup = Game_Actor.prototype.setup;
+  Game_Actor.prototype.setup = function (actorId) {
+    _Game_Actor_setup.call(this, actorId);
+    this.clearItemRestriction();
+  };
 
-    const _Game_Action_testApply = Game_Action.prototype.testApply;
-    Game_Action.prototype.testApply = function (target) {
-        if (this.isAlreadyUsed(target)) return false;
-        if (!this.isTargetActorValid(target)) return false;
-        return _Game_Action_testApply.call(this, target);
-    };
+  //-----------------------------------------------------------------------------
+  // Game_Action
+  //
 
-    Game_Action.prototype.isAlreadyUsed = function (target) {
-        const item = this.item();
-        return this.isItem() && item.meta.justOnce && target.isAlreadyUsed(item);
-    };
+  const _Game_Action_testApply = Game_Action.prototype.testApply;
+  Game_Action.prototype.testApply = function (target) {
+    if (this.isAlreadyUsed(target)) return false;
+    if (!this.isTargetActorValid(target)) return false;
+    return _Game_Action_testApply.call(this, target);
+  };
 
-    Game_Action.prototype.isTargetActorValid = function (target) {
-        if (target.isActor() && this.isItem()) {
-            const item = this.item();
-            if (item.meta.targetActor) {
-                const targetActors = item.meta.targetActor.split(' ').map(Number);
-                return targetActors.contains(target.actorId());
-            }
-        }
-        return true;
-    };
+  Game_Action.prototype.isAlreadyUsed = function (target) {
+    let item = this.item();
+    return this.isItem() && item.meta.justOnce && target.isAlreadyUsed(item);
+  };
 
-    const _Game_Action_apply = Game_Action.prototype.apply;
-    Game_Action.prototype.apply = function (target) {
-        _Game_Action_apply.call(this, target);
-        if (target.result().isHit()) this.setAlreadyUsed(target);
-    };
+  Game_Action.prototype.isTargetActorValid = function (target) {
+    if (target.isActor() && this.isItem()) {
+      let item = this.item();
+      if (item.meta.targetActor) {
+        let targetActors = item.meta.targetActor.split(' ').map(Number);
+        return targetActors.contains(target.actorId());
+      }
+    }
+    return true;
+  };
 
-    Game_Action.prototype.setAlreadyUsed = function (target) {
-        const item = this.item();
-        if (this.isItem() && item.meta.justOnce) target.setAlreadyUsed(item);
-    };
+  const _Game_Action_apply = Game_Action.prototype.apply;
+  Game_Action.prototype.apply = function (target) {
+    _Game_Action_apply.call(this, target);
+    if (target.result().isHit()) this.setAlreadyUsed(target);
+  };
 
-    //-----------------------------------------------------------------------------
-    // PluginManager
-    //
+  Game_Action.prototype.setAlreadyUsed = function (target) {
+    let item = this.item();
+    if (this.isItem() && item.meta.justOnce) target.setAlreadyUsed(item);
+  };
 
-    PluginManager.registerCommand(pluginName, "clearItemRestriction", args => {
-        const actor = $gameActors.actor(Number(args.actorId));
-        if (actor) actor.clearItemRestriction();
-    });
+  //-----------------------------------------------------------------------------
+  // PluginManager
+  //
 
-    //-----------------------------------------------------------------------------
-    // Window_BattleActor
-    //
+  PluginManager.registerCommand(pluginName, "clearItemRestriction", args => {
+    const arr = [args.actorId];
+    let actor = $gameActors.actor(+arr[0]);
+    if (actor) actor.clearItemRestriction();
+  });
 
-    Window_BattleActor.prototype.playOkSound = function () { };
+  //-----------------------------------------------------------------------------
+  // Window_BattleActor
+  //
 
-    Window_BattleActor.prototype.playBuzzerSound = function () { };
+  Window_BattleActor.prototype.playOkSound = function () {
+  };
 
-    //-----------------------------------------------------------------------------
-    // Scene_Battle
-    //
+  Window_BattleActor.prototype.playBuzzerSound = function () {
+  };
 
-    const _Scene_Battle_onActorOk = Scene_Battle.prototype.onActorOk;
-    Scene_Battle.prototype.onActorOk = function () {
-        const action = BattleManager.inputtingAction();
-        const actor = this._actorWindow.actor(this._actorWindow.index());
-        if (action.isAlreadyUsed(actor) || !action.isTargetActorValid(actor)) {
-            SoundManager.playBuzzer();
-            this._actorWindow.activate();
-        } else {
-            SoundManager.playOk();
-            _Scene_Battle_onActorOk.call(this);
-        }
-    };
+  //-----------------------------------------------------------------------------
+  // Scene_Battle
+  //
+
+  const _Scene_Battle_onActorOk = Scene_Battle.prototype.onActorOk;
+  Scene_Battle.prototype.onActorOk = function () {
+    let action = BattleManager.inputtingAction();
+    let actor = this._actorWindow.actor();
+    if (action.isAlreadyUsed(actor) || !action.isTargetActorValid(actor)) {
+      SoundManager.playBuzzer();
+      this._actorWindow.activate();
+    } else {
+      SoundManager.playOk();
+      _Scene_Battle_onActorOk.call(this);
+    }
+  };
 
 })();
