673cb6786d5248e9a547f3ff87a67795e50c852d
munokura <munokura@users.noreply.github.com>
2020-11-07 11:34:21 +0900

    パラメーターの仕様を原作に戻す(MZのdecimalsにバグが有るため、パラメーターの指定を解除）

--- MNKR_TMBattlerExMZ.js
+++ MNKR_TMBattlerExMZ.js
@@ -1,6 +1,6 @@
-﻿/*
+/*
  * --------------------------------------------------
- * MNKR_TMBattlerExMZ Ver.1.0.2
+ * MNKR_TMBattlerExMZ Ver.2.0.0
  * Copyright (c) 2020 Munokura
  * This software is released under the MIT license.
  * http://opensource.org/licenses/mit-license.php
@@ -21,49 +21,8 @@
 /*:
  * @target MZ
  * @url https://raw.githubusercontent.com/munokura/MNKR-MZ-plugins/master/MNKR_TMBattlerExMZ.js
- * @plugindesc 敵キャラに遠近感や息づかいの表現を追加します。
+ * @plugindesc v2.0.0 敵キャラに遠近感や息づかいの表現を追加します。
  * @author tomoaky (改変 munokura)
- *
- * @param baseY
- * @text 基本Y座標
- * @type number
- * @desc 拡大率が等倍になるY座標。
- * 初期値: 400
- * @default 400
- *
- * @param breathH
- * @text 息づかい拡大率
- * @type number
- * @desc 息づかいの大きさ。
- * 初期値: 5
- * @default 5
- *
- * @param mirrorRate
- * @text 左右反転の確率
- * @type number
- * @min 0
- * @max 100
- * @desc 左右反転の確率。
- * 初期値: 40（ 0 ～ 100 ）
- * @default 40
- *
- * @param breathStop
- * @text 行動不能時の息停止
- * @type boolean
- * @on 止める
- * @off 止めない
- * @desc 行動不能時に息づかいを止める。
- * 初期値: true
- * @default true
- *
- * @param shakeEffect
- * @text ダメージ時揺れ
- * @type boolean
- * @on 揺れエフェクト
- * @off 点滅エフェクト
- * @desc 点滅エフェクトを揺れエフェクトに差し替える。
- * 初期値: true
- * @default true
  *
  * @help
  * 使い方:
@@ -85,7 +44,7 @@
  *     拡大率を個別に設定します、このタグがある敵キャラには遠近感の表現が
  *     適用されません。
  *
- *   <breathH:5>
+ *   <breathH:0.05>
  *     息づかいの大きさを個別に設定します。
  *
  *   <noMirror>
@@ -97,20 +56,63 @@
  *   https://ja.osdn.net/projects/opensource/wiki/licenses%2FMIT_license
  *   作者に無断で改変、再配布が可能で、
  *   利用形態(商用、18禁利用等)についても制限はありません。
+ * 
+ * 
+ * @param baseY
+ * @text 基本Y座標
+ * @type number
+ * @desc 拡大率が等倍になるY座標。
+ * 初期値: 400
+ * @default 400
+ *
+ * @param breathH
+ * @text 息づかい拡大率
+ * @desc 息づかいの大きさ。
+ * 初期値: 0.05
+ * @default 0.05
+ *
+ * @param mirrorRate
+ * @text 左右反転の確率
+ * @desc 左右反転の確率。
+ * 初期値: 0.40（ 0 ～ 1 ）
+ * @default 0.40
+ *
+ * @param breathStop
+ * @text 行動不能時の息停止
+ * @type boolean
+ * @on 止める
+ * @off 止めない
+ * @desc 行動不能時に息づかいを止める。
+ * 初期値: true
+ * @default true
+ *
+ * @param shakeEffect
+ * @text ダメージ時揺れ
+ * @type boolean
+ * @on 揺れエフェクト
+ * @off 点滅エフェクト
+ * @desc 点滅エフェクトを揺れエフェクトに差し替える。
+ * 初期値: true
+ * @default true
  */
 
+var Imported = Imported || {};
+Imported.TMBattlerEx = true;
 
 (() => {
 
   "use strict";
 
   const pluginName = document.currentScript.src.split("/").pop().replace(/\.js$/, "");
-  const parameters = PluginManager.parameters(pluginName);
-  const BaseY = Number(parameters['baseY'] || 400);
-  const BreathH = Number(parameters['breathH'] || 5);
-  const MirrorRate = Number(parameters['mirrorRate'] || 40);
-  const BreathStop = String(parameters['breathStop']) === 'true';
-  const ShakeEffect = String(parameters['shakeEffect']) === 'true';
+
+  var TMPlugin = TMPlugin || {};
+  TMPlugin.BattlerEx = {};
+  TMPlugin.BattlerEx.Parameters = PluginManager.parameters(pluginName);
+  TMPlugin.BattlerEx.BaseY = +(TMPlugin.BattlerEx.Parameters['baseY'] || 400);
+  TMPlugin.BattlerEx.BreathH = +(TMPlugin.BattlerEx.Parameters['breathH'] || 0.05);
+  TMPlugin.BattlerEx.MirrorRate = +(TMPlugin.BattlerEx.Parameters['mirrorRate'] || 0.4);
+  TMPlugin.BattlerEx.BreathStop = TMPlugin.BattlerEx.Parameters['breathStop'] === '1';
+  TMPlugin.BattlerEx.ShakeEffect = TMPlugin.BattlerEx.Parameters['shakeEffect'] === '1';
 
   //-----------------------------------------------------------------------------
   // Sprite_Enemy
@@ -119,9 +121,9 @@
   const _Sprite_Enemy_initialize = Sprite_Enemy.prototype.initialize;
   Sprite_Enemy.prototype.initialize = function (battler) {
     _Sprite_Enemy_initialize.call(this, battler);
-    let r = +(battler.enemy().meta.scale || this.y / (BaseY * 2) + 0.5);
+    let r = +(battler.enemy().meta.scale || this.y / (TMPlugin.BattlerEx.BaseY * 2) + 0.5);
     this._baseScale = new Point(r, r);
-    if (!$gameSystem.isSideView() && Math.random() < (MirrorRate / 100) &&
+    if (!$gameSystem.isSideView() && Math.random() < TMPlugin.BattlerEx.MirrorRate &&
       !battler.enemy().meta.noMirror) {
       this._baseScale.x = 0 - r;
       this._stateIconSprite.scale.x = -1;
@@ -137,7 +139,7 @@
   };
 
   Sprite_Enemy.prototype.updateScaleEx = function () {
-    if (BreathStop && !this._enemy.canMove()) return;
+    if (TMPlugin.BattlerEx.BreathStop && !this._enemy.canMove()) return;
     this._breathCount++;
     if (this._breathCount >= this._breathMax) {
       this._breathMax = Math.randomInt(90) + 90;
@@ -145,14 +147,14 @@
     }
     let scaleX = this._baseScale.x;
     let scaleY = this._baseScale.y;
-    let bh = +((this._enemy.enemy().meta.breathH || BreathH) / 100);
+    let bh = +(this._enemy.enemy().meta.breathH || TMPlugin.BattlerEx.BreathH);
     scaleY += Math.sin(Math.PI * this._breathCount / (this._breathMax / 2)) * bh;
     this.scale.set(scaleX, scaleY);
   };
 
   const _Sprite_Enemy_updateBlink = Sprite_Enemy.prototype.updateBlink;
   Sprite_Enemy.prototype.updateBlink = function () {
-    if (ShakeEffect) {
+    if (TMPlugin.BattlerEx.ShakeEffect) {
       let ed = this._effectDuration;
       this.rotation = ed % 4 < 2 ? ed / 200 : 0 - ed / 200;
     } else {
