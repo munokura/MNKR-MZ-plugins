2960e05f67fd484be7a60bbfa5119c43fd740946
munokura <munokura@users.noreply.github.com>
2020-11-17 19:19:56 +0900

    MITライセンスのリンクが死んでいたので、置換

--- MNKR_TextCodeHelpMZ.js
+++ MNKR_TextCodeHelpMZ.js
@@ -52,7 +52,7 @@
  *
  * 利用規約:
  *   MITライセンスです。
- *   https://ja.osdn.net/projects/opensource/wiki/licenses%2FMIT_license
+ *   https://licenses.opensource.jp/MIT/MIT.html
  *   作者に無断で改変、再配布が可能で、
  *   利用形態（商用、18禁利用等）についても制限はありません。
  */
