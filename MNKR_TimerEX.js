4237b062c63a9271404c14b531a17ed4bb44448e
munokura <munokura@users.noreply.github.com>
2020-12-12 17:18:12 +0900

    タイマー表示切り替えスイッチ機能追加

--- MNKR_TimerEX.js
+++ MNKR_TimerEX.js
@@ -1,6 +1,6 @@
 /*
  * --------------------------------------------------
- * MNKR_TimerEX Ver.1.0.3
+ * MNKR_TimerEX Ver.1.1.0
  * Copyright (c) 2020 Munokura
  * This software is released under the MIT license.
  * http://opensource.org/licenses/mit-license.php
@@ -10,13 +10,12 @@
 /*:
  * @target MZ MV
  * @url https://raw.githubusercontent.com/munokura/MNKR-MZ-plugins/master/MNKR_TimerEX.js
- * @plugindesc タイマーをカウントアップ・ダウンできるようにします。
+ * @plugindesc タイマーをカウントアップ・ダウン、表示・非表示を切り替えできるようにします。
  * @author munokura
  *
  * @help
  * タイマーをカウントアップ・ダウンできるようにします。
- * 発動スイッチがOFFの場合、通常のカウントダウンの動作をします。
- * 発動スイッチがONの場合、カウントアップの動作をします。
+ * タイマーをスイッチで表示・非表示を切り替えられます。
  * 
  * カウントアップ・ダウンに関わらず、タイマー停止時の値を変数に保存します。
  * 保存される値はフレーム値(1フレーム:1/60秒)です。
@@ -32,13 +31,21 @@
  *   利用形態（商用、18禁利用等）についても制限はありません。
  * 
  * 
- * @param Count Up Switch
+ * @param countUpSwitch
  * @text カウントアップ発動スイッチ
  * @type switch
- * @desc 指定スイッチがON時カウントアップ。OFF時カウントダウン。無指定の場合、常にカウントアップになります。
+ * @desc 指定スイッチがON時カウントアップ。OFF時カウントダウン。
+ * 無指定の場合、常にカウントアップになります。
  * @default 0
  *
- * @param Count Stop Value
+ * @param hideSwitch
+ * @text タイマー非表示スイッチ
+ * @type switch
+ * @desc 指定スイッチがON時タイマーを非表示。OFF時に表示。
+ * 無指定の場合、常に表示になります。
+ * @default 0
+ *
+ * @param countStopValue
  * @text 保存変数ID
  * @type variable
  * @desc タイマーを停止した時の値を保存する変数ID。取得値はフレーム値(1フレーム:1/60秒)
@@ -50,8 +57,9 @@
 
     const pluginName = document.currentScript.src.split("/").pop().replace(/\.js$/, "");
     const parameters = PluginManager.parameters(pluginName);
-    const countUpSwitch = Number(parameters['Count Up Switch'] || 0);
-    const countStopValue = Number(parameters['Count Stop Value'] || 0);
+    const countUpSwitch = Number(parameters['countUpSwitch'] || 0);
+    const hideSwitch = Number(parameters['hideSwitch'] || 0);
+    const countStopValue = Number(parameters['countStopValue'] || 0);
 
     const _Game_Timer_update = Game_Timer.prototype.update;
     Game_Timer.prototype.update = function (sceneActive) {
@@ -67,6 +75,15 @@
         _Game_Timer_stop.call(this);
         if (countStopValue) {
             $gameVariables.setValue(countStopValue, this._frames);
+        }
+    };
+
+    const _Sprite_Timer_redraw = Sprite_Timer.prototype.redraw;
+    Sprite_Timer.prototype.redraw = function () {
+        if ((!$gameSwitches.value(hideSwitch) || hideSwitch === 0)) {
+            _Sprite_Timer_redraw.call(this);
+        } else {
+            this.bitmap.clear();
         }
     };
 
