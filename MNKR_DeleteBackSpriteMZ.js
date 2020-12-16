bbe4e755b8616354a34fcba08d6fb7bb1eca6a31
munokura <munokura@users.noreply.github.com>
2020-12-17 08:19:24 +0900

    ファイル名変更

--- MNKR_DeleteBackSpriteMZ.js
+++ MNKR_DeleteBackSpriteMZ.js
@@ -1,6 +1,6 @@
 /*
  * --------------------------------------------------
- * MNKR_DeleteBackSprite Ver.0.0.1
+ * MNKR_DeleteBackSpriteMZ Ver.0.0.1
  * Copyright (c) 2020 Munokura
  * This software is released under the MIT license.
  * http://opensource.org/licenses/mit-license.php
@@ -9,7 +9,7 @@
 
 /*:
  * @target MZ
- * @url https://raw.githubusercontent.com/munokura/MNKR-MZ-plugins/master/MNKR_DeleteBackSprite.js
+ * @url https://raw.githubusercontent.com/munokura/MNKR-MZ-plugins/master/MNKR_DeleteBackSpriteMZ.js
  * @plugindesc コマンド個別の黒い背景を全て非表示にします。
  * @author munokura
  *
