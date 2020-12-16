3d613a17a70c1ad908d0dd6b328aa78c94bfd41f
munokura <36838778+munokura@users.noreply.github.com>
2020-12-16 15:40:03 +0900

    alpha

---
+++ MNKR_TMTopFixMZ_DarkPlasma_FormationPatch.js
@@ -0,0 +1,46 @@
+/*
+ * --------------------------------------------------
+ * MNKR_TMTopFixMZ_DarkPlasma_FormationPatch Ver.0.0.1
+ * Copyright (c) 2020 Munokura
+ * This software is released under the MIT license.
+ * http://opensource.org/licenses/mit-license.php
+ * --------------------------------------------------
+ */
+
+/*:
+ * @target MZ
+ * @url https://raw.githubusercontent.com/munokura/MNKR-MZ-plugins/master/MNKR_TMTopFixMZ_DarkPlasma_FormationPatch.js
+ * @plugindesc MNKR_TMTopFixMZの機能をDarkPlasma_Formationに適用するパッチ
+ * @author munokura
+ * @base DarkPlasma_Formation
+ * @base MNKR_TMTopFixMZ
+ * @orderAfter MNKR_TMTopFixMZ
+ *
+ * @help
+ * DarkPlasma_Formation のシーンにおいて、
+ * パーティの先頭にいるアクターの並び替えを禁止します。
+ *
+ * 使い方:
+ * DarkPlasma_Formation と MNKR_TMTopFixMZ をプラグイン管理でONにします。
+ * MNKR_TMTopFixMZ_DarkPlasma_FormationPatch をプラグイン管理でONにし、
+ * MNKR_TMTopFixMZ の下側に配置してください。
+ * 
+ * 例
+ *   DarkPlasma_Formation
+ *   MNKR_TMTopFixMZ
+ *   MNKR_TMTopFixMZ_DarkPlasma_FormationPatch
+ */
+
+(() => {
+    'use strict';
+
+    const _Window_FormationSelect_isCurrentItemEnabled = Window_FormationSelect.prototype.isCurrentItemEnabled;
+    Window_FormationSelect.prototype.isCurrentItemEnabled = function () {
+        console.log('isCurrentItemEnabled');
+        if ($gameSystem.isTopFix() && this.index() === 0) {
+            return false;
+        }
+        return _Window_FormationSelect_isCurrentItemEnabled.call(this);
+    };
+
+})();
