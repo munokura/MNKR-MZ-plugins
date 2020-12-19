2a2c760129af9f62d67d562cb6d9bbdfcb67bfe8
munokura <munokura@users.noreply.github.com>
2020-12-19 21:29:08 +0900

    ヘルプ追記

--- MNKR_SwitchGameoverMusic.js
+++ MNKR_SwitchGameoverMusic.js
@@ -1,6 +1,6 @@
 /*
  * --------------------------------------------------
- * MNKR_SwitchGameoverMusic Ver.0.0.1
+ * MNKR_SwitchGameoverMusic Ver.0.0.2
  * Copyright (c) 2020 Munokura
  * This software is released under the MIT license.
  * http://opensource.org/licenses/mit-license.php
@@ -11,9 +11,12 @@
  * @target MZ MV
  * @url https://raw.githubusercontent.com/munokura/MNKR-MZ-plugins/master/MNKR_SwitchGameoverMusic.js
  * @plugindesc 指定スイッチがON時ゲームオーバー時に直前のBGMが続きます。
+ * @author munokura
  * 
  * @help
  * 指定スイッチがON時ゲームオーバー時に直前のBGMが続きます。
+ * 
+ * 指定スイッチを指定しない場合、常にゲームオーバー時に直前のBGMが続きます。
  *
  *
  * 利用規約:
