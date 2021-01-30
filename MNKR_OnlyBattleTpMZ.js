9c52f278e9d6a5e714db54f9655205a38c88c0f3
munokura <munokura@users.noreply.github.com>
2021-01-30 15:00:26 +0900

    alpha

---
+++ MNKR_OnlyBattleTpMZ.js
@@ -0,0 +1,39 @@
+/*
+ * --------------------------------------------------
+ * MNKR_OnlyBattleTpMZ Ver.0.0.1
+ * Copyright (c) 2021 Munokura
+ * This software is released under the MIT license.
+ * http://opensource.org/licenses/mit-license.php
+ * --------------------------------------------------
+ */
+/*:
+ * @target MZ
+ * @url https://raw.githubusercontent.com/munokura/MNKR-MZ-plugins/master/MNKR_OnlyBattleTpMZ.js
+ * @plugindesc 戦闘画面の以外のTP表示を消します。
+ * @author munokura
+ *
+ * @help
+ * 戦闘画面の以外のTP表示を消します。
+ * 
+ * 
+ * 利用規約:
+ *   MITライセンスです。
+ *   https://licenses.opensource.jp/MIT/MIT.html
+ *   作者に無断で改変、再配布が可能で、
+ *   利用形態（商用、18禁利用等）についても制限はありません。
+ */
+(() => {
+    'use strict';
+
+    const _Window_StatusBase_placeBasicGauges = Window_StatusBase.prototype.placeBasicGauges;
+    Window_StatusBase.prototype.placeBasicGauges = function (actor, x, y) {
+        const isSceneMenu = SceneManager._scene.constructor.name;
+        if (['Scene_Battle'].includes(isSceneMenu)) {
+            _Window_StatusBase_placeBasicGauges.call(this, actor, x, y);
+        } else {
+            this.placeGauge(actor, "hp", x, y);
+            this.placeGauge(actor, "mp", x, y + this.gaugeLineHeight());
+        }
+    };
+
+})();
