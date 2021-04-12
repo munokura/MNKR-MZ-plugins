a8c0331ce72823eaa613cb2d59424745e83023ec
munokura <munokura@users.noreply.github.com>
2021-04-13 08:13:27 +0900

    alpha

---
+++ MNKR_ReplaceMenu.js
@@ -0,0 +1,53 @@
+/*
+ * --------------------------------------------------
+ * MNKR_ReplaceMenu.js
+ *   Ver.0.0.1
+ * Copyright (c) 2021 Munokura
+ * This software is released under the MIT license.
+ * http://opensource.org/licenses/mit-license.php
+ * --------------------------------------------------
+ */
+
+/*:
+ * @target MZ MV
+ * @url https://raw.githubusercontent.com/munokura/MNKR-MZ-plugins/master/MNKR_ReplaceMenu.js
+ * @plugindesc メニューを開く動作を一時的にコモンイベントに置き換えます。
+ * @author munokura
+ *
+ * @help
+ * メニューを開く動作を一時的にコモンイベントに置き換えます。
+ * 変数の値が0の時、通常のメニューを開きます。
+ * 
+ *
+ * 利用規約:
+ *   MITライセンスです。
+ *   https://licenses.opensource.jp/MIT/MIT.html
+ *   作者に無断で改変、再配布が可能で、
+ *   利用形態（商用、18禁利用等）についても制限はありません。
+ *
+ *
+ * @param variableCommon
+ * @text コモンイベント指定変数ID
+ * @type variable
+ * @default 0
+ * @desc 変数の数値をIDとしたコモンイベントを実行します。
+ */
+
+(() => {
+  "use strict";
+
+  const pluginName = document.currentScript.src.split("/").pop().replace(/\.js$/, "");
+  const parameters = PluginManager.parameters(pluginName);
+  const variableCommon = Number(parameters['variableCommon'] || 0);
+
+  const _Scene_Map_callMenu = Scene_Map.prototype.callMenu;
+  Scene_Map.prototype.callMenu = function () {
+    const commonId = Number($gameVariables.value(variableCommon));
+    if (commonId > 0) {
+      $gameTemp.reserveCommonEvent(commonId)
+    } else {
+      _Scene_Map_callMenu.call(this);
+    }
+  };
+
+})();
