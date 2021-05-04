76147e4f8fcaeba64e29ba960607b96b10a71aa4
munokura <munokura@users.noreply.github.com>
2021-05-04 13:30:12 +0900

    alpha2

--- MNKR_MKR_EventGaugeMZ.js
+++ MNKR_MKR_EventGaugeMZ.js
@@ -80,7 +80,7 @@
  * @target MZ
  * @url https://raw.githubusercontent.com/munokura/MNKR-MZ-plugins/master/MNKR_MKR_EventGaugeMZ.js
  *
- * @plugindesc (v1.2.3) イベントゲージプラグイン
+ * @plugindesc イベントゲージ表示
  * @author マンカインド (改変 munokura)
  *
  * @help
