025cf9e700df7aac99e68972e1033d9fb86e9d5f
munokura <munokura@users.noreply.github.com>
2021-05-14 04:57:19 +0900

    リファクタ

--- MNKR_EnemyStateIconOffset.js
+++ MNKR_EnemyStateIconOffset.js
@@ -1,7 +1,7 @@
 /*
  * --------------------------------------------------
  * MNKR_EnemyStateIconOffset.js
- *   Ver.0.0.1
+ *   Ver.0.0.2
  * Copyright (c) 2021 Munokura
  * This software is released under the MIT license.
  * http://opensource.org/licenses/mit-license.php
@@ -32,18 +32,15 @@
  *   利用形態（商用、18禁利用等）についても制限はありません。
  */
 
-
-
 (() => {
   "use strict";
 
   const _Sprite_Enemy_updateStateSprite = Sprite_Enemy.prototype.updateStateSprite;
   Sprite_Enemy.prototype.updateStateSprite = function () {
+    _Sprite_Enemy_updateStateSprite.call(this);
     const offSetY = Number($dataEnemies[this._battler._enemyId].meta.MNKR_EnemyStateIconOffset || 0);
-    if (offSetY === 0) {
-      _Sprite_Enemy_updateStateSprite.call(this);
-    } else {
-      this._stateIconSprite.y = -Math.round((this.bitmap.height + 40) * 0.9) + offSetY;
+    if (offSetY !== 0) {
+      this._stateIconSprite.y = this._stateIconSprite.y + offSetY;
     }
   };
 
