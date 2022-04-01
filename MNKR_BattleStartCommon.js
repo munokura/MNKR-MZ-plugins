5d55aff4db2ae47b8c5fefeae1fee67444d173e5
munokura <munokura@users.noreply.github.com>
2022-04-01 14:51:54 +0900

    リファクタリング

--- MNKR_BattleStartCommon.js
+++ MNKR_BattleStartCommon.js
@@ -1,7 +1,7 @@
 /*
  * --------------------------------------------------
  * MNKR_BattleStartCommon.js
- *   Ver.2.0.3
+ *   Ver.2.0.4
  * Copyright (c) 2020 Munokura
  * This software is released under the MIT license.
  * http://opensource.org/licenses/mit-license.php
@@ -11,7 +11,7 @@
 /*:
  * @target MZ MV
  * @url https://raw.githubusercontent.com/munokura/MNKR-MZ-plugins/master/MNKR_BattleStartCommon.js
- * @plugindesc v2.0.2 戦闘開始時のメッセージをコモンイベントに置き換えます。
+ * @plugindesc 戦闘開始時のメッセージをコモンイベントに置き換えます。
  * @author munokura
  * 
  * @help
@@ -46,9 +46,6 @@
  * }
  *
  *
- * プラグインコマンドはありません。
- *
- *
  * 利用規約:
  *   MITライセンスです。
  *   https://licenses.opensource.jp/MIT/MIT.html
@@ -56,13 +53,13 @@
  *   利用形態（商用、18禁利用等）についても制限はありません。
  * 
  *
- * @param Variable Id
+ * @param variableId
  * @text 指定変数
  * @type variable
- * @desc コモンイベントを指定する変数ID。この変数の値のIDのコモンイベントを実行します。
- * @default 0
+ * @desc コモンイベントを指定する変数ID。変数値がIDのコモンイベントを実行します。なしの場合、値が0と同じ動作になります。
+ * @default 1
  * 
- * @param Value Zero
+ * @param valueZero
  * @text 変数値が0の処理
  * @type boolean
  * @on 表示
@@ -76,15 +73,15 @@
 
     const pluginName = document.currentScript.src.split("/").pop().replace(/\.js$/, "");
     const parameters = PluginManager.parameters(pluginName);
-    const variableId = Number(parameters['Variable Id'] || 0);
-    const valueZero = String(parameters['Value Zero']) === 'true';
+    const PRM_variableId = Number(parameters['variableId'] || 0);
+    const PRM_valueZero = parameters['valueZero'] === 'true';
 
     const _BattleManager_displayStartMessages = BattleManager.displayStartMessages;
     BattleManager.displayStartMessages = function () {
-        if (valueZero && $gameVariables.value(variableId) === 0) {
+        if (PRM_valueZero && $gameVariables.value(PRM_variableId) === 0) {
             _BattleManager_displayStartMessages.call(this);
         } else {
-            $gameTemp.reserveCommonEvent($gameVariables.value(variableId));
+            $gameTemp.reserveCommonEvent($gameVariables.value(PRM_variableId));
         }
     };
 
