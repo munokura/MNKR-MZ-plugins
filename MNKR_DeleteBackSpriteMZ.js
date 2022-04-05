53859b50c9f8330e4750c7be39161aeb4a8a5346
munokura <munokura@users.noreply.github.com>
2022-04-05 13:19:52 +0900

    リファクタリング

--- MNKR_DeleteBackSpriteMZ.js
+++ MNKR_DeleteBackSpriteMZ.js
@@ -1,7 +1,7 @@
 /*
  * --------------------------------------------------
  * MNKR_DeleteBackSpriteMZ.js
- *   Ver.0.1.0
+ *   Ver.0.1.1
  * Copyright (c) 2020 Munokura
  * This software is released under the MIT license.
  * http://opensource.org/licenses/mit-license.php
@@ -26,7 +26,7 @@
  *   利用形態（商用、18禁利用等）についても制限はありません。
  * 
  * 
- * @param WindowScrollable
+ * @param windowScrollable
  * @text スクロール背景無効化
  * @type boolean
  * @on 無効化
@@ -34,7 +34,7 @@
  * @default true
  * @desc スクロール可能なウィンドウの選択肢の背景を無効化
  * 
- * @param WindowHelp
+ * @param windowHelp
  * @text ヘルプ背景無効化
  * @type boolean
  * @on 無効化
@@ -42,7 +42,7 @@
  * @default true
  * @desc ヘルプウィンドウの背景を無効化
  * 
- * @param WindowMessage
+ * @param windowMessage
  * @text メッセージ背景無効化
  * @type boolean
  * @on 無効化
@@ -50,7 +50,7 @@
  * @default true
  * @desc 文章表示ウィンドウの背景を無効化
  * 
- * @param WindowMapName
+ * @param windowMapName
  * @text マップ名背景無効化
  * @type boolean
  * @on 無効化
@@ -59,7 +59,7 @@
  * @desc マップ名表示の背景を無効化
  * 推奨：有効化
  * 
- * @param WindowBattleLog
+ * @param windowBattleLog
  * @text バトルログ背景無効化
  * @type boolean
  * @on 無効化
@@ -73,16 +73,16 @@
 
   const pluginName = document.currentScript.src.split("/").pop().replace(/\.js$/, "");
   const parameters = PluginManager.parameters(pluginName);
-  const WindowScrollable = parameters['WindowScrollable'] === 'true';
-  const WindowHelp = parameters['WindowHelp'] === 'true';
-  const WindowMessage = parameters['WindowMessage'] === 'true';
-  const WindowMapName = parameters['WindowMapName'] === 'true';
-  const WindowBattleLog = parameters['WindowBattleLog'] === 'true';
+  const PRM_windowScrollable = parameters['windowScrollable'] === 'true';
+  const PRM_windowHelp = parameters['windowHelp'] === 'true';
+  const PRM_windowMessage = parameters['windowMessage'] === 'true';
+  const PRM_windowMapName = parameters['windowMapName'] === 'true';
+  const PRM_windowBattleLog = parameters['windowBattleLog'] === 'true';
 
   const _Window_Scrollable_initialize = Window_Scrollable.prototype.initialize;
   Window_Scrollable.prototype.initialize = function (rect) {
     _Window_Scrollable_initialize.call(this, rect);
-    if (WindowScrollable) {
+    if (PRM_windowScrollable) {
       this._contentsBackSprite.alpha = 0;
     }
   };
@@ -90,7 +90,7 @@
   const _Window_Help_initialize = Window_Help.prototype.initialize;
   Window_Help.prototype.initialize = function (rect) {
     _Window_Help_initialize.call(this, rect);
-    if (WindowHelp) {
+    if (PRM_windowHelp) {
       this._contentsBackSprite.alpha = 0;
     }
   };
@@ -98,7 +98,7 @@
   const _Window_Message_initialize = Window_Message.prototype.initialize;
   Window_Message.prototype.initialize = function (rect) {
     _Window_Message_initialize.call(this, rect);
-    if (WindowMessage) {
+    if (PRM_windowMessage) {
       this._contentsBackSprite.alpha = 0;
     }
   };
@@ -106,7 +106,7 @@
   const _Window_MapName_initialize = Window_MapName.prototype.initialize;
   Window_MapName.prototype.initialize = function (rect) {
     _Window_MapName_initialize.call(this, rect);
-    if (WindowMapName) {
+    if (PRM_windowMapName) {
       this._contentsBackSprite.alpha = 0;
     }
   };
@@ -114,7 +114,7 @@
   const _Window_BattleLog_initialize = Window_BattleLog.prototype.initialize;
   Window_BattleLog.prototype.initialize = function (rect) {
     _Window_BattleLog_initialize.call(this, rect);
-    if (WindowBattleLog) {
+    if (PRM_windowBattleLog) {
       this._contentsBackSprite.alpha = 0;
     }
   };
