a8feb6f55163cc49f7bbe03cb9bb1b711e007a9e
munokura <munokura@users.noreply.github.com>
2022-04-08 16:34:44 +0900

    リファクタリング：関数化

--- MNKR_AddSkillWtype.js
+++ MNKR_AddSkillWtype.js
@@ -1,7 +1,7 @@
 /*
  * --------------------------------------------------
  * MNKR_AddSkillWtype.js
- *   Ver.0.0.1
+ *   Ver.0.0.2
  * Copyright (c) 2022 Munokura
  * This software is released under the MIT license.
  * http://opensource.org/licenses/mit-license.php
@@ -11,12 +11,15 @@
 /*:
  * @target MZ MV
  * @url https://raw.githubusercontent.com/munokura/MNKR-MZ-plugins/master/MNKR_AddSkillWtype.js
- * @plugindesc 装備している防具に、スキルの必要武器タイプを追加できます。
+ * @plugindesc 装備に、スキルの必要武器タイプを追加できます。
  * @author munokura
  *
  * @help
- * 装備している防具に、スキル＞必要武器＞必要武器タイプ1・2を追加できます。
- * 防具のメモ欄に下記のタグを入れてください。
+ * 装備に、スキル＞必要武器＞必要武器タイプ1・2を追加できます。
+ * 装備で追加した場合、スキルで必要とされる武器タイプの武器を装備していなくても、
+ * スキル使用可能になります。
+ * 
+ * 武器・防具のメモ欄に下記のタグを入れてください。
  * 
  * <MNKR_AddSkillWtype:武器タイプID>
  * 
@@ -42,15 +45,7 @@
   const _Game_Actor_isSkillWtypeOk = Game_Actor.prototype.isSkillWtypeOk;
   Game_Actor.prototype.isSkillWtypeOk = function (skill) {
     const equipsArray = this.equips();
-    let armorMetaArray = [];
-    for (let i = 0; i < equipsArray.length; i++) {
-      if (equipsArray[i]) {
-        const hasArmorMeta = equipsArray[i].meta.MNKR_AddSkillWtype || false;
-        if (hasArmorMeta) {
-          armorMetaArray = armorMetaArray.concat(equipsArray[i].meta.MNKR_AddSkillWtype.split(',').map(Number));
-        }
-      }
-    }
+    const armorMetaArray = equipsMeta(equipsArray);
     if (armorMetaArray.length === 0) {
       return _Game_Actor_isSkillWtypeOk.call(this, skill);
     }
@@ -61,5 +56,18 @@
     }
     return _Game_Actor_isSkillWtypeOk.call(this, skill);
   };
+
+  function equipsMeta(equipsArray) {
+    let equipMetaArray = [];
+    for (let i = 0; i < equipsArray.length; i++) {
+      if (equipsArray[i]) {
+        const hasEquipsMeta = equipsArray[i].meta.MNKR_AddSkillWtype || false;
+        if (hasEquipsMeta) {
+          equipMetaArray = equipMetaArray.concat(equipsArray[i].meta.MNKR_AddSkillWtype.split(',').map(Number));
+        }
+      }
+    }
+    return equipMetaArray;
+  }
 
 })();
